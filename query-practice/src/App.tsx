3bc1ea7ddd0957296ef9e92d7bc2653299644fdc
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-03 20:35:59 -0400

    feat: Implement data fetching with TanStack React Query and style with Tailwind CSS.

--- query-practice/src/App.tsx
+++ query-practice/src/App.tsx
@@ -1,7 +1,34 @@
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
+interface ITodoData {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
 function App() {
-  return <></>
+  const { data, isLoading, error } = useQuery<ITodoData[]>({
+    queryKey: ['todos'],
+    queryFn: () =>
+      fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
+        res.json(),
+      ),
+  })
+
+  if (isLoading) return <div className='text-2xl'>Loading...</div>
+  if (error) return <div className='text-2xl'>Error: {error.message}</div>
+
+  return (
+    <main className='flex flex-col items-center justify-center'>
+      <ul className='flex flex-col items-center justify-center'>
+        {data?.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </main>
+  )
 }
 
 export default App
